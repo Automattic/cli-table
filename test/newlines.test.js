0c1a623b6bc41b1380ce536162114b08d85e940b
sonnym <michaud.sonny@gmail.com>
2012-09-09 17:05:06 -0400

    handles newlines in header cells

---
+++ test/newlines.test.js
@@ -0,0 +1,33 @@
+/**
+ * Module requirements.
+ */
+
+require('should');
+
+var Table = require('cli-table');
+
+/**
+ * Tests.
+ */
+
+module.exports = {
+  'test table with newlines in headers': function() {
+    var table = new Table({
+        head: ['Test', "1\n2\n3"]
+      , style: {
+            'padding-left': 1
+          , 'padding-right': 1
+        }
+    });
+
+    var expected = [
+        '┌──────┬───┐'
+      , '│ Test │ 1 │'
+      , '│      │ 2 │'
+      , '│      │ 3 │'
+      , '└──────┴───┘'
+    ];
+
+    table.toString().should.eql(expected.join("\n"));
+  }
+};
