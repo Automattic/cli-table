0a340a756d03beffb1489eb4256202592e57155e
Gabriel Sambarino <gabriel.sambarino@automattic.com>
2020-12-06 14:38:58 +0100

    Add new Jest test files

---
+++ __tests__/newlines.test.js
@@ -0,0 +1,90 @@
+/**
+ * Module requirements.
+ */
+
+var Table = require('../');
+
+/**
+ * Tests.
+ */
+
+module.exports = {
+  'test table with newlines in headers': function() {
+    var table = new Table({
+        head: ['Test', "1\n2\n3"]
+      , style: {
+            'padding-left': 1
+          , 'padding-right': 1
+          , head: []
+          , border: []
+        }
+    });
+
+    var expected = [
+        '┌──────┬───┐'
+      , '│ Test │ 1 │'
+      , '│      │ 2 │'
+      , '│      │ 3 │'
+      , '└──────┴───┘'
+    ];
+
+    expect(table.toString()).toEqual(expected.join("\n"));
+  },
+
+  'test column width is accurately reflected when newlines are present': function() {
+    var table = new Table({ head: ['Test\nWidth'], style: {head:[], border:[]} });
+    expect(table.width).toBe(9);
+  },
+
+  'test newlines in body cells': function() {
+    var table = new Table({style: {head:[], border:[]}});
+
+    table.push(["something\nwith\nnewlines"]);
+
+    var expected = [
+        '┌───────────┐'
+      , '│ something │'
+      , '│ with      │'
+      , '│ newlines  │'
+      , '└───────────┘'
+    ];
+
+    expect(table.toString()).toEqual(expected.join("\n"));
+  },
+
+  'test newlines in vertical cell header and body': function() {
+    var table = new Table({ style: {'padding-left':0, 'padding-right':0, head:[], border:[]} });
+
+    table.push(
+        {'v\n0.1': 'Testing\nsomething cool'}
+    );
+
+    var expected = [
+        '┌───┬──────────────┐'
+      , '│v  │Testing       │'
+      , '│0.1│something cool│'
+      , '└───┴──────────────┘'
+    ];
+
+    expect(table.toString()).toEqual(expected.join("\n"));
+  },
+
+  'test newlines in cross table header and body': function() {
+    var table = new Table({ head: ["", "Header\n1"], style: {'padding-left':0, 'padding-right':0, head:[], border:[]} });
+
+    table.push({ "Header\n2": ['Testing\nsomething\ncool'] });
+
+    var expected = [
+        '┌──────┬─────────┐'
+      , '│      │Header   │'
+      , '│      │1        │'
+      , '├──────┼─────────┤'
+      , '│Header│Testing  │'
+      , '│2     │something│'
+      , '│      │cool     │'
+      , '└──────┴─────────┘'
+    ];
+
+    expect(table.toString()).toEqual(expected.join("\n"));
+  }
+};
