288c86e23244b063f646448016f635f4b415d91c
Gabriel Sambarino <gabriel.sambarino@automattic.com>
2020-12-06 14:22:26 +0100

    Fix typo in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 // babel.config.js
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  '@babel/preset-typescript',
+  presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      '@babel/preset-typescript',
+  ],
 };
