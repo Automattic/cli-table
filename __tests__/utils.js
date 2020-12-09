b753284930e6d082261f4579fd21ea8b8944cff0
Gabriel Sambarino <gabriel.sambarino@automattic.com>
2020-12-09 15:41:16 +0100

    More linting on tests

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,31 +1,31 @@
 /**
  * Internal dependencies
  */
-import {pad} from '../lib/utils';
+const pad = require( '../lib/utils' ).pad;
 
 describe('utils',() => {
     describe('pad', () => {
-        it("pad('hello',6,' ', right) == 'hello '", () => {
+        it('pad(\'hello\',6,\' \', right) == \'hello \'', () => {
             expect(pad('hello', 6, ' ', 'right')).toBe('hello ');
         });
 
-        it("pad('hello',7,' ', left) == '  hello'", () => {
+        it('pad(\'hello\',7,\' \', left) == \'  hello\'', () => {
             expect(pad('hello', 7, ' ', 'left')).toBe('  hello');
         });
 
-        it("pad('hello',8,' ', both) == ' hello  '", function () {
+        it('pad(\'hello\',8,\' \', both) == \' hello  \'', function () {
             expect(pad('hello', 8, ' ', 'both')).toBe(' hello  ');
         });
 
-        it("pad('hello',9,' ', both) == '  hello  '", function () {
+        it('pad(\'hello\',9,\' \', both) == \'  hello  \'', function () {
             expect(pad('hello', 9, ' ', 'both')).toBe('  hello  ');
         });
 
-        it("pad('yw',4,' ', both) == ' yw '", function () {
+        it('pad(\'yw\',4,\' \', both) == \' yw \'', function () {
             expect(pad('yw', 4, ' ', 'both')).toBe(' yw ');
         });
 
-        it("pad('hello', 2, ' ', right) == 'hello'", function () {
+        it('pad(\'hello\', 2, \' \', right) == \'hello\'', function () {
             expect(pad('hello', 2, ' ', 'right')).toBe('hello');
         });
     });
